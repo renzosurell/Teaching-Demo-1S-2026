cdaf82dfda011214fb31e2b425d70aa9159daeba
enzo <renzo.surell@mseuf.edu.ph>
2026-07-31 23:23:52 +0800

    Variables

--- C++/myFirstProgram.cpp
+++ C++/myFirstProgram.cpp
@@ -3,30 +3,20 @@ using namespace std;
 
 int main() {
 
-    //adding newline using \n
-    cout << "Hi there! \n";
-    cout << "I am learning C++";
-    return 0;
+    //declaring/initializing variables
+    int whole_number = 6;
+    double num1 = 6.99999;
+    char singleCharacter = 'A';
+    string word = "asdASD123!@#";
 
-    /* 
+    //declaring/initializing constant variable
+    const double pi = 3.1415;
 
-    //alternative for adding newline
-    cout << "Hi there!" << "\n";
-    cout << "I am learning C++";
-    return 0;
-    
-    //alternative for adding newline
-    cout << "Hi there!" << endl;
-    cout << "I am learning C++";
-    return 0;
+    //printing values
+    cout << "The value of the variable \"whole_number\" is: " << whole_number << "\n";
+    cout << "The value of the variable \"num1\" is: " << num1 << "\n";
+    cout << "The value of the variable \"singleCharacter\" is: " << singleCharacter << "\n";
+    cout << "The value of the variable \"word\" is: " << word << "\n";
+    cout << "The value of the variable \"pi\" is: " << pi << "\n";
 
-    //printing number
-    cout << 6;
-    return 0;
-
-    //printing expression
-    cout << (6 + 7);
-    return 0;
-
-    */
 }
--- Java/myFirstProgram.java
+++ Java/myFirstProgram.java
@@ -1,24 +1,6 @@
 public class myFirstProgram {
     public static void main(String[] args)
     {
-
-    //adding newline using \n
-    System.out.print("Hi there! \n");
-    System.out.print("I am learning Java");
-
-    /* 
-
-    //alternative for adding newline
-    System.out.println("Hi there!");
-    System.out.print("I am learning Java");
-
-    //printing number
-    System.out.print(6);
-
-    //printing expression
-    System.out.print(6 + 7);
-
-    */
-
+        
     }
 }
