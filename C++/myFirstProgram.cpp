7b97598c58592a3b1d75bfd41b56d47876c28645
enzo <renzo.surell@mseuf.edu.ph>
2026-08-01 13:38:17 +0800

    Assignment Operators

--- C++/myFirstProgram.cpp
+++ C++/myFirstProgram.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 int main() {
 
-    int x = 3;
+    int x = 6;          //the assignment operator
+    cout << "The value of x after initialization:" << x << "\n";
+    cout << "\n";
 
-    cout << (x << 1) << "\n";   //Bitwise left-shift
-    cout << (x >> 1) << "\n";   //Bitwise right-shift
-    cout << (x | 1) << "\n";   //Bitwise Or
-    cout << (x & 1) << "\n";   //Bitwise And
-    cout << (x ^ 1) << "\n";   //Bitwise XOr
+    cout << "The value of x applying increment operator: " << ++x << "\n";
+    cout << "The value of x applying decrement operator: " << --x << "\n";
+
+    cout << "\n";
+
+    cout << "The value of x applying post-increment operator: " << x++ << "\n";
+    cout << "The value of x after previous statement: " << x << "\n";
+
+    cout << "\n";
+
+    cout << "The value of x applying post-decrement operator: " << x-- << "\n";
+    cout << "The value of x after previous statement: " << x << "\n";
 
     return 0;
 }
